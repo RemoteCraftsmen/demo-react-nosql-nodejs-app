331fba7bb0cf25674acec1b64b43bc8505347eae
Piotr Skowroński <piotr.skowronski@remotecraftsmen.com>
2019-03-21 17:22:21 +0100

    Merged readme into master

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,13 @@ import rootReducer from './reducers';
 const middleware = [thunk];
 const initialState = {};
 
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : x => x;
 const store = createStore(
     rootReducer,
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        devTools
     )
 );
 
