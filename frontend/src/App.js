8d483337c3899b8bb2d5bbbee044e77ec7029a80
Dawid Pstrak <dawid.pstrak@remotecraftsmen.com>
2021-07-21 18:01:55 +0200

    Routes refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,27 +29,6 @@ class App extends Component {
     render() {
         const { classes, isAuthenticated } = this.props;
 
-        if (isAuthenticated) {
-            return (
-                <Router>
-                    <SiteNavigation />
-                    <div style={{ padding: 4 }}>
-                        <Grid container className={classes.root} spacing={8} justify="center">
-                            <Grid item xs={12} sm={10} md={8}>
-                                <Switch>
-                                    <Route exact path="/" component={Login} />
-                                    <Route exact path="/login" component={Login} />
-                                    <Route exact path="/register" component={Register} />
-                                    <Route exact path="/tasks" component={TaskList} />
-                                    <Route component={NotFound} />
-                                </Switch>
-                            </Grid>
-                        </Grid>
-                    </div>
-                </Router>
-            );
-        }
-
         return (
             <Router>
                 <SiteNavigation />
@@ -60,6 +39,7 @@ class App extends Component {
                                 <Route exact path="/" component={Login} />
                                 <Route exact path="/login" component={Login} />
                                 <Route exact path="/register" component={Register} />
+                                {isAuthenticated && <Route exact path="/tasks" component={TaskList} />}
                                 <Route component={NotFound} />
                             </Switch>
                         </Grid>
